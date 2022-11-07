8332ef7d149d6acff5c43e96af3e46d5dfaf1b1e
Nils Hartmann <nils@nilshartmann.net>
2022-11-07 06:05:51 +0100

    Namen geandert

--- src/60_infer.ts
+++ src/60_infer.ts
@@ -2,15 +2,21 @@ export default undefined;
 
 // Beispiel: Pattern Matching mit infer von Strings
 //
-//   - parsePath soll ein Objekt zurückliefern,
+//   - parseUrl soll ein Objekt zurückliefern,
 //     In dem die Platzhalter aus der übergebenen URLs als Keys und die Werte als Strings enthalten sind
 //
 // - Segments type => wie eine rekursive Funktion, in der wir eine Liste für das Ergebnis übergeben
 
-declare function parsePath<S extends string>(url: S): any;
-
 const path = "/api/v1/:userId/likes/:likeId";
 
-const r = parsePath(path);
+// Erwarteter Ziel-Typ:
+// {
+//   userId: string;
+//   likeId: string;
+// }
+
+declare function parseUrl<S extends string>(url: S): any;
+
+const r = parseUrl(path);
 r.likeId; // OK
 r.personId; // ERROR
--- src/60_x_infer.ts
+++ src/60_x_infer.ts
@@ -1,6 +1,12 @@
+export default undefined;
 import { ToObject, ToUnion } from "./util-types";
 
-export default undefined;
+// Beispiel: Pattern Matching mit infer von Strings
+//
+//   - parseUrl soll ein Objekt zurückliefern,
+//     In dem die Platzhalter aus der übergebenen URLs als Keys und die Werte als Strings enthalten sind
+//
+// - Segments type => wie eine rekursive Funktion, in der wir eine Liste für das Ergebnis übergeben
 
 type IsArg<A extends string> = A extends `:${infer ARG}` ? ARG : never;
 
@@ -11,13 +17,13 @@ type Segments<
   ? Segments<R, [...RESULT, IsArg<L>]>
   : [...RESULT, IsArg<URL>];
 
-declare function parsePath<S extends string>(
+declare function parseUrl<S extends string>(
   path: S
 ): ToObject<ToUnion<Segments<S>>>;
 
 const path = "/api/v1/:userId/likes/:likeId";
 
-const r = parsePath(path);
+const r = parseUrl(path);
 r.likeId; // OK
 r.userId.toLowerCase(); // OK
 r.personId; // ERROR Property 'personId' does not exist
